7416596f4339c869c80f6b8f219237be1e1a345d
Rehan <rehannajam2@gmail.com>
2024-11-04 01:23:13 -0800

    add README file to document project setup and usage

--- back-end/index.js
+++ back-end/index.js
@@ -1,23 +1,28 @@
 // Imports 
 import express from 'express';
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
 
 // App Config
 const app = express();
 const PORT = 4000;
-const url = process.emit.MONGO_URL ?? '';
+dotenv.config({path: './.env'});
+
+const url = process.env.MONGO_URL ?? '';
 const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'testDB';
-
+ 
 async function main() {
   await client.connect();
   const db = client.db(dbName);
-  const collection = db.collection('documents');
-
-  return 'done.';
+  db.collection('documents');
+  console.log('Connected to the database 🚀');
 }
+
+// Middleware
+main().catch(console.error);
 
 app.get('/', (req, res) => {
     res.json("I love docker!");
